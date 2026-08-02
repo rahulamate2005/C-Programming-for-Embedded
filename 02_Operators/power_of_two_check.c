2c74d167af954586ac5781fb6da793c6acbc9c47
rahulamate2005 <rahulamate2005@gmail.com>
2026-08-02 22:32:42 +0530

    Add mixed operator expression program

---
+++ 02_Operators/power_of_two_check.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+
+    scanf("%d",&n);
+
+
+    if(n > 0 && (n & (n-1)) == 0)
+    {
+        printf("Power of Two");
+    }
+    else
+    {
+        printf("Not Power of Two");
+    }
+
+
+    return 0;
+}
