25f64c72e577babe56a07d17599e47394bec3440
rahulamate2005 <rahulamate2005@gmail.com>
2026-08-02 22:06:39 +0530

    Add assignment operators program

---
+++ 02_Operators/assignment_operators.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main()
+{
+    int x = 10;
+
+    x += 5;
+    printf("+= %d\n", x);
+
+    x -= 3;
+    printf("-= %d\n", x);
+
+    x *= 2;
+    printf("*= %d\n", x);
+
+    x /= 4;
+    printf("/= %d\n", x);
+
+    x %= 3;
+    printf("%%= %d\n", x);
+
+    return 0;
+}
