4f23b749dfad31e4e952a8930e653f2a86847573
rahulamate2005 <rahulamate2005@gmail.com>
2026-09-01 16:07:59 +0530

    Added swap using function

---
+++ 05_Functions/07_swap_using_function.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+void swap(int *a, int *b)
+{
+    int temp;
+
+    temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+int main()
+{
+    int a, b;
+
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+
+    printf("Before swap: a = %d, b = %d\n", a, b);
+
+    swap(&a, &b);
+
+    printf("After swap: a = %d, b = %d", a, b);
+
+    return 0;
+}
