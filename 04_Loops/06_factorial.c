a7f7fd5f5dde40c0347b297f789c8a86d8e9a6b1
rahulamate2005 <rahulamate2005@gmail.com>
2026-09-01 15:51:10 +0530

    Added factorial program

---
+++ 04_Loops/06_factorial.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    int factorial = 1;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    for (int i = 1; i <= n; i++)
+    {
+        factorial = factorial * i;
+    }
+
+    printf("Factorial = %d", factorial);
+
+    return 0;
+}
