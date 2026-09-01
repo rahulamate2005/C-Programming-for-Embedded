306d2d478c54380f28131384ec5ce585ce7c9651
rahulamate2005 <rahulamate2005@gmail.com>
2026-09-01 19:44:58 +0530

    Added power using recursion

---
+++ 06_Recursion/03_power_recursion.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int power(int base, int exponent)
+{
+    if (exponent == 0)
+        return 1;
+
+    return base * power(base, exponent - 1);
+}
+
+int main()
+{
+    int base, exponent;
+
+    printf("Enter base and exponent: ");
+    scanf("%d %d", &base, &exponent);
+
+    printf("Result = %d", power(base, exponent));
+
+    return 0;
+}
