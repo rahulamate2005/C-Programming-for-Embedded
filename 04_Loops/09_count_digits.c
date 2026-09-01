50ef764afc4e5f3e2472a15843fe0eee225782b7
rahulamate2005 <rahulamate2005@gmail.com>
2026-09-01 15:55:21 +0530

    Added count digits program

---
+++ 04_Loops/09_count_digits.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    int count = 0;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    while (n != 0)
+    {
+        n = n / 10;
+        count++;
+    }
+
+    printf("Number of digits = %d", count);
+
+    return 0;
+}
