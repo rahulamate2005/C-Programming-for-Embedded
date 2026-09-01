98bfb69abb7eaec7d6bdc506631f76f3c3bf1061
rahulamate2005 <rahulamate2005@gmail.com>
2026-09-01 15:49:49 +0530

    Added palindrome number program

---
+++ 04_Loops/03_palindrome_number.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    int original;
+    int reverse = 0;
+    int remainder;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    original = n;
+
+    while (n != 0)
+    {
+        remainder = n % 10;
+        reverse = reverse * 10 + remainder;
+        n = n / 10;
+    }
+
+    if (original == reverse)
+    {
+        printf("Palindrome number");
+    }
+    else
+    {
+        printf("Not a palindrome number");
+    }
+
+    return 0;
+}
